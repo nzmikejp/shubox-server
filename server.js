1a6a7dbf2ddc43c18cd4507ad177b8c6cfcc46d0
khoitran20042000@gmail.com <khoitran20042000@gmail.com>
2020-09-28 13:43:08 +1300

    final test, ready for submit

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.use(fileUpload())
+app.use(express.static('public'))
 
 app.use(logger('dev'))
 
